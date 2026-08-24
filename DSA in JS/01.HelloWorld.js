66bdf05168a7a030b953da87c05a207a6e317e0b
Saurabh Chauhan <saurabh2chauhanp@gmail.com>
2026-08-24 10:22:36 +0530

    Day 01: Hello World Function

---
+++ DSA in JS/01.HelloWorld.js
@@ -0,0 +1,6 @@
+// LC 2667: Create Hello World Function
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+
+function createHelloWorld(){
+    return "Hello World";
+}

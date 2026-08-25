f738713628a16bfe5c60d40e1e98db7e4f603e24
Saurabh Chauhan <saurabh2chauhanp@gmail.com>
2026-08-25 13:46:14 +0530

    Day 09: Return Length of Arguments Passed

---
+++ DSA in JS/09.ArgumentLength.js
@@ -0,0 +1,6 @@
+// LC 2703: Return length of Arguments Passed
+// Write a function argumentsLength that returns the count of arguments passed to it.
+function argumentsLength(){
+    return arguments.length;
+};
+
